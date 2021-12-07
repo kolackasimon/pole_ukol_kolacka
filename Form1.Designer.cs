3f330e4534537ce78d56301543f93fef6000aa0f
Šimon Koláčka <kolackas.04@spst.cz>
2021-12-07 21:26:59 +0100

    Přidejte soubory projektu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace pole_ukol_kolacka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.radioButtonVzestupne = new System.Windows.Forms.RadioButton();
+            this.radioButtonSestupne = new System.Windows.Forms.RadioButton();
+            this.textBoxVstup = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(116, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(115, 298);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(98, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vygenerované pole";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(529, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Změněné pole";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 21;
+            this.listBox2.Location = new System.Drawing.Point(533, 91);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(115, 298);
+            this.listBox2.TabIndex = 2;
+            // 
+            // radioButtonVzestupne
+            // 
+            this.radioButtonVzestupne.AutoSize = true;
+            this.radioButtonVzestupne.BackColor = System.Drawing.Color.Transparent;
+            this.radioButtonVzestupne.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButtonVzestupne.ForeColor = System.Drawing.Color.White;
+            this.radioButtonVzestupne.Location = new System.Drawing.Point(315, 199);
+            this.radioButtonVzestupne.Name = "radioButtonVzestupne";
+            this.radioButtonVzestupne.Size = new System.Drawing.Size(115, 27);
+            this.radioButtonVzestupne.TabIndex = 4;
+            this.radioButtonVzestupne.TabStop = true;
+            this.radioButtonVzestupne.Text = "Vzestupně";
+            this.radioButtonVzestupne.UseVisualStyleBackColor = false;
+            this.radioButtonVzestupne.CheckedChanged += new System.EventHandler(this.radioButtonVzestupne_CheckedChanged);
+            // 
+            // radioButtonSestupne
+            // 
+            this.radioButtonSestupne.AutoSize = true;
+            this.radioButtonSestupne.BackColor = System.Drawing.Color.Transparent;
+            this.radioButtonSestupne.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButtonSestupne.ForeColor = System.Drawing.Color.White;
+            this.radioButtonSestupne.Location = new System.Drawing.Point(315, 264);
+            this.radioButtonSestupne.Name = "radioButtonSestupne";
+            this.radioButtonSestupne.Size = new System.Drawing.Size(102, 27);
+            this.radioButtonSestupne.TabIndex = 5;
+            this.radioButtonSestupne.TabStop = true;
+            this.radioButtonSestupne.Text = "Sestupně";
+            this.radioButtonSestupne.UseVisualStyleBackColor = false;
+            this.radioButtonSestupne.CheckedChanged += new System.EventHandler(this.radioButtonSestupne_CheckedChanged);
+            // 
+            // textBoxVstup
+            // 
+            this.textBoxVstup.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxVstup.Location = new System.Drawing.Point(370, 115);
+            this.textBoxVstup.Name = "textBoxVstup";
+            this.textBoxVstup.Size = new System.Drawing.Size(100, 26);
+            this.textBoxVstup.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(274, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Vstup N";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(315, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 62);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxVstup);
+            this.Controls.Add(this.radioButtonSestupne);
+            this.Controls.Add(this.radioButtonVzestupne);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Úkol Pole 1 - Koláčka";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.RadioButton radioButtonVzestupne;
+        private System.Windows.Forms.RadioButton radioButtonSestupne;
+        private System.Windows.Forms.TextBox textBoxVstup;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pole_ukol_kolacka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButtonVzestupne_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonVzestupne.Checked)
+            {
+                listBox2.Items.Clear();
+                Array.Sort(pole);
+                foreach (int s in pole)
+                {
+                    listBox2.Items.Add(s);
+                }
+            }
+        }
+
+        private void radioButtonSestupne_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonSestupne.Checked)
+            {
+                listBox2.Items.Clear();
+                Array.Sort(pole);
+                Array.Reverse(pole);
+                foreach (int s in pole)
+                {
+                    listBox2.Items.Add(s);
+                }
+            }
+        }
+
+        int[] pole;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rnd = new Random();
+            int vstup = int.Parse(textBoxVstup.Text);
+            pole = new int[vstup];
+            for (int i = 0; i < pole.Length; i++)
+            {
+                pole[i] = rnd.Next(1, 51);
+                listBox1.Items.Add(pole[i].ToString());
+            }
+            if (radioButtonSestupne.Checked)
+            {
+                listBox2.Items.Clear();
+                Array.Sort(pole);
+                Array.Reverse(pole);
+                foreach (int s in pole)
+                {
+                    listBox2.Items.Add(s);
+                }
+            }
+            if (radioButtonVzestupne.Checked)
+            {
+                listBox2.Items.Clear();
+                Array.Sort(pole);
+                foreach (int s in pole)
+                {
+                    listBox2.Items.Add(s);
+                }
+            }
+        }
+    }
+}
