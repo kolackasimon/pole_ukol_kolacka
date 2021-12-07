b32ca83fd4deae7c3c45bad894bfc85d38c3454b
Šimon Koláčka <kolackas.04@spst.cz>
2021-12-07 21:28:51 +0100

    final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,10 +44,11 @@
             // 
             this.listBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(116, 91);
+            this.listBox1.ItemHeight = 27;
+            this.listBox1.Location = new System.Drawing.Point(155, 112);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(115, 298);
+            this.listBox1.Size = new System.Drawing.Size(152, 355);
             this.listBox1.TabIndex = 0;
             // 
             // label1
@@ -56,9 +57,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(98, 51);
+            this.label1.Location = new System.Drawing.Point(131, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 23);
+            this.label1.Size = new System.Drawing.Size(221, 31);
             this.label1.TabIndex = 1;
             this.label1.Text = "Vygenerované pole";
             // 
@@ -68,9 +70,10 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(529, 51);
+            this.label2.Location = new System.Drawing.Point(705, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 23);
+            this.label2.Size = new System.Drawing.Size(169, 31);
             this.label2.TabIndex = 3;
             this.label2.Text = "Změněné pole";
             // 
@@ -78,10 +81,11 @@
             // 
             this.listBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 21;
-            this.listBox2.Location = new System.Drawing.Point(533, 91);
+            this.listBox2.ItemHeight = 27;
+            this.listBox2.Location = new System.Drawing.Point(711, 112);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(115, 298);
+            this.listBox2.Size = new System.Drawing.Size(152, 355);
             this.listBox2.TabIndex = 2;
             // 
             // radioButtonVzestupne
@@ -90,9 +94,10 @@
             this.radioButtonVzestupne.BackColor = System.Drawing.Color.Transparent;
             this.radioButtonVzestupne.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.radioButtonVzestupne.ForeColor = System.Drawing.Color.White;
-            this.radioButtonVzestupne.Location = new System.Drawing.Point(315, 199);
+            this.radioButtonVzestupne.Location = new System.Drawing.Point(420, 245);
+            this.radioButtonVzestupne.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButtonVzestupne.Name = "radioButtonVzestupne";
-            this.radioButtonVzestupne.Size = new System.Drawing.Size(115, 27);
+            this.radioButtonVzestupne.Size = new System.Drawing.Size(150, 35);
             this.radioButtonVzestupne.TabIndex = 4;
             this.radioButtonVzestupne.TabStop = true;
             this.radioButtonVzestupne.Text = "Vzestupně";
@@ -105,9 +110,10 @@
             this.radioButtonSestupne.BackColor = System.Drawing.Color.Transparent;
             this.radioButtonSestupne.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.radioButtonSestupne.ForeColor = System.Drawing.Color.White;
-            this.radioButtonSestupne.Location = new System.Drawing.Point(315, 264);
+            this.radioButtonSestupne.Location = new System.Drawing.Point(420, 325);
+            this.radioButtonSestupne.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButtonSestupne.Name = "radioButtonSestupne";
-            this.radioButtonSestupne.Size = new System.Drawing.Size(102, 27);
+            this.radioButtonSestupne.Size = new System.Drawing.Size(134, 35);
             this.radioButtonSestupne.TabIndex = 5;
             this.radioButtonSestupne.TabStop = true;
             this.radioButtonSestupne.Text = "Sestupně";
@@ -117,9 +123,10 @@
             // textBoxVstup
             // 
             this.textBoxVstup.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxVstup.Location = new System.Drawing.Point(370, 115);
+            this.textBoxVstup.Location = new System.Drawing.Point(493, 142);
+            this.textBoxVstup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxVstup.Name = "textBoxVstup";
-            this.textBoxVstup.Size = new System.Drawing.Size(100, 26);
+            this.textBoxVstup.Size = new System.Drawing.Size(132, 30);
             this.textBoxVstup.TabIndex = 6;
             // 
             // label3
@@ -128,9 +135,10 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(274, 115);
+            this.label3.Location = new System.Drawing.Point(365, 142);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 23);
+            this.label3.Size = new System.Drawing.Size(105, 31);
             this.label3.TabIndex = 7;
             this.label3.Text = "Vstup N";
             // 
@@ -138,20 +146,21 @@
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(315, 327);
+            this.button1.Location = new System.Drawing.Point(420, 402);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 62);
+            this.button1.Size = new System.Drawing.Size(185, 76);
             this.button1.TabIndex = 8;
-            this.button1.Text = "Start";
+            this.button1.Text = "Generovat";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBoxVstup);
@@ -163,6 +172,7 @@
             this.Controls.Add(this.listBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
